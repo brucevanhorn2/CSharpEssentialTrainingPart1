cf310def83f136f7301f9c0e872e4140101cf2e9
Bruce Van Horn <brucevanhorn@me.com>
2017-10-21 11:55:13 -0700

    adding 5-2

---
+++ CH05/05-02/begin/SchoolApp/SchoolLibrary/Teacher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    class Teacher : Person
+    {
+        public string Subject { get; set; }
+    }
+}
---
+++ CH05/05-02/done/SchoolApp/SchoolLibrary/Teacher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Teacher : Person
+    {
+        public string Subject { get; set; }
+    }
+}
