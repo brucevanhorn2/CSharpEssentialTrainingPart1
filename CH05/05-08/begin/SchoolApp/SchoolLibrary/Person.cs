e15c96586649afaf50ffe6392e13211ebcac1d76
Bruce Van Horn <brucevanhorn@me.com>
2017-10-21 15:25:52 -0700

    added 5-8

---
+++ CH05/05-08/begin/SchoolApp/SchoolLibrary/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public abstract float ComputeGradeAverage();
+
+        public virtual string SendMessage(string message)
+        {
+            var sb = new StringBuilder();
+            var timeStamp = string.Format("Sent on {0:D} at {0:t}", DateTime.Now);
+            sb.AppendLine(timeStamp);
+            sb.AppendLine("");
+            sb.AppendLine("Dear " + FirstName + ",");
+            sb.AppendLine(message);
+            return sb.ToString();
+        }
+    }
+}
