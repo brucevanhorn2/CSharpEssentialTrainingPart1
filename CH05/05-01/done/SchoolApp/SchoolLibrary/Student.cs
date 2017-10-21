b537f1199651053817bde5679a4c0f7f5ff8fadc
Bruce Van Horn <brucevanhorn@me.com>
2017-10-20 21:16:50 -0700

    Added 5-1 done

---
+++ CH05/05-01/done/SchoolApp/SchoolLibrary/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Student : Person
+    {
+        public enum GradeLevels { Freshman, Sophomore, Junior, Senior }
+        public GradeLevels GradeLevel { get; set; }
+    }
+}
