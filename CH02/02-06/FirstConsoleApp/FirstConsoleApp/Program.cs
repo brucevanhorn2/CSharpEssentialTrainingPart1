69702819139ef755ad0d17b00bb4c20af83ddcf1
Bruce Van Horn <brucevanhorn@me.com>
2017-10-20 20:30:05 -0700

    adding chapter 2

---
+++ CH02/02-06/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Essential Training Part 1!");
+            Console.WriteLine("Press the enter key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
