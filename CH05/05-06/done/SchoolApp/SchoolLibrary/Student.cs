2567eb78a132aba146983ed4a7743ef66541a434
Bruce Van Horn <brucevanhorn@me.com>
2017-10-21 12:48:57 -0700

    adding sections on virtual methods

---
+++ CH05/05-06/done/SchoolApp/SchoolLibrary/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Student : Person
+    {
+        public enum GradeLevels { Freshman, Sophomore, Junior, Senior }
+        public GradeLevels GradeLevel { get; set; }
+
+        public override float ComputeGradeAverage()
+        {
+            return 4.0f;
+        }
+
+        public override string SendMessage(string message)
+        {
+            var original = base.SendMessage(message);
+            var sb = new StringBuilder(original);
+            sb.AppendLine("This message is private and confidential.");
+            return sb.ToString();
+        }
+    }
+}
